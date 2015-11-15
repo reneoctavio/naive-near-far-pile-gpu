789f487bda49881488a3bd00949ec3f04de27f96
Rene Octavio Queiroz Dias <reneoctavio@gmail.com>
2015-11-15 20:34:03 -0200

    Simplified

--- src/definitions.cuh
+++ src/definitions.cuh
@@ -9,7 +9,7 @@
 #define DEFINITIONS_CUH_
 
 #ifndef DEBUG_MSG
-#define DEBUG_MSG 1
+#define DEBUG_MSG 0
 #endif
 
 #include <cusp/csr_matrix.h>
@@ -43,8 +43,6 @@
 #include <thrust/iterator/permutation_iterator.h>
 #include <thrust/iterator/zip_iterator.h>
 
-#include "expand.cuh"
-
 typedef typename cusp::csr_matrix<int, int, cusp::host_memory> HCsrMatrix;
 typedef typename cusp::csr_matrix<int, int, cusp::device_memory> DCsrMatrix;
 
@@ -53,12 +51,17 @@ typedef typename DVector::iterator DVectorIterator;
 
 typedef typename thrust::permutation_iterator<DVectorIterator, DVectorIterator> PermIteratorIdx;
 
-typedef typename thrust::tuple<DVectorIterator, DVectorIterator> TupleTentDist;
-typedef typename thrust::zip_iterator<TupleTentDist> ZipTupleTentDistIterator;
+typedef typename thrust::tuple<DVectorIterator, DVectorIterator> Tuple2TentDist;
+typedef typename thrust::zip_iterator<Tuple2TentDist> ZipTuple2TentDistIterator;
 
-struct is_light {
+typedef typename thrust::tuple<DVectorIterator, DVectorIterator, DVectorIterator> Tuple3TentDist;
+typedef typename thrust::zip_iterator<Tuple3TentDist> ZipTuple3TentDistIterator;
+
+struct is_within_range {
 	const int k_upper_distance;
-	is_light(int _upper) : k_upper_distance(_upper) {}
+	is_within_range(int _upper) :
+			k_upper_distance(_upper) {
+	}
 
 	__host__ __device__
 	bool operator()(const int& x) {
@@ -66,9 +69,22 @@ struct is_light {
 	}
 };
 
+struct is_less_than_lower {
+	const int k_lower_distance;
+	is_less_than_lower(int _lower) :
+			k_lower_distance(_lower) { }
+
+	__host__ __device__
+	bool operator()(const int& x) {
+		return x < k_lower_distance;
+	}
+};
+
 struct is_heavy {
 	const int k_upper_distance;
-	is_heavy(int _upper) : k_upper_distance(_upper) {}
+	is_heavy(int _upper) :
+			k_upper_distance(_upper) {
+	}
 
 	__host__ __device__
 	bool operator()(const int& x) {
@@ -76,12 +92,16 @@ struct is_heavy {
 	}
 };
 
-struct is_light_tuple {
+struct is_within_range_tuple {
 	const int k_upper;
-	is_light_tuple(int _upper) : k_upper(_upper) {}
-	is_light_tuple() : k_upper(0) {}
+	is_within_range_tuple(int _upper) :
+			k_upper(_upper) {
+	}
+	is_within_range_tuple() :
+			k_upper(0) {
+	}
 
-	template <typename Tuple>
+	template<typename Tuple>
 	__host__ __device__ bool operator()(Tuple tuple) {
 		int tent_dist = thrust::get<1>(tuple);
 		return tent_dist <= k_upper;
@@ -89,24 +109,40 @@ struct is_light_tuple {
 };
 
 struct is_tent_dist_greater {
-  int* distance_begin;
-  is_tent_dist_greater(int* _distance_begin)
-      : distance_begin(_distance_begin) {}
+	int* distance_begin;
+	is_tent_dist_greater(int* _distance_begin) :
+			distance_begin(_distance_begin) {
+	}
 
-  template <typename Tuple>
-  __host__ __device__ bool operator()(Tuple tuple) {
-    int cur_dist = distance_begin[thrust::get<0>(tuple)];
-    return thrust::get<1>(tuple) > cur_dist;
-  }
+	template<typename Tuple>
+	__host__ __device__ bool operator()(Tuple tuple) {
+		int cur_dist = distance_begin[thrust::get<0>(tuple)];
+		return thrust::get<1>(tuple) > cur_dist;
+	}
 };
 
-struct is_dist_non_set {
+struct is_tent_dist_greater_far {
 	int* distance_begin;
-	is_dist_non_set(int* _distance_begin)
-	: distance_begin(_distance_begin) {}
+	is_tent_dist_greater_far(int* _distance_begin) :
+			distance_begin(_distance_begin) {
+	}
+
+	template<typename Tuple>
+	__host__ __device__ bool operator()(Tuple tuple) {
+		int cur_dist = distance_begin[thrust::get<0>(tuple)];
+		return thrust::get<1>(tuple) > cur_dist;
+	}
+};
+
+struct is_dist_set_in_near_pile {
+	int* distance_begin;
+	const int k_upper;
+	is_dist_set_in_near_pile(int* _distance_begin, int _upper_distance) :
+			distance_begin(_distance_begin), k_upper(_upper_distance) {
+	}
 
 	__host__ __device__ bool operator()(const int& x) {
-		return distance_begin[x] == INT_MAX;
+		return distance_begin[x] <= k_upper;
 	}
 };
 
--- src/main.cu
+++ src/main.cu
@@ -8,7 +8,8 @@
 #include "definitions.cuh"
 
 int main(int argc, char* argv[]) {
-	if (argc != 4) return 1;
+	if (argc != 4)
+		return 1;
 
 	cudaDeviceProp prop;
 	cudaGetDeviceProperties(&prop, 0);
@@ -38,80 +39,178 @@ int main(int argc, char* argv[]) {
 
 	DVector d_out_edges_count(k_num_of_vertices + 1);
 	thrust::adjacent_difference(thrust::device, d_graph.row_offsets.begin(),
-								d_graph.row_offsets.end(), d_out_edges_count.begin());
+			d_graph.row_offsets.end(), d_out_edges_count.begin());
 	d_out_edges_count.erase(d_out_edges_count.begin());
 
-	DVector d_current_out_edges_count(k_num_of_vertices);
-	DVector d_current_out_edges_index(k_num_of_vertices);
+	DVector d_current_out_edges_count(k_num_of_vertices + 1, 0);
+	DVector d_current_out_edges_index(k_num_of_vertices + 1, 0);
 
 	d_distance[0] = 0;
 	d_near_set.push_back(0);
 
-	bool is_work_left = true;
-
 	int i = 0;
-	while (is_work_left) {
-		int upper_distance = (i + 1) * k_delta;
-#if DEBUG_MSG
-		std::cout << "Iteration #: " << i << "\nUpper Distance: " << upper_distance << std::endl;
-#endif
+	int lower_distance = i * k_delta;
+	int upper_distance = (i + 1) * k_delta;
+	while (!d_far_pile.empty() || !d_near_set.empty()) {
+		std::cout << "Iteration #: " << i << "\nLower: " << lower_distance
+				<< " Upper: " << upper_distance << std::endl;
+
 		while (!d_near_set.empty()) {
 			// Copy current vertices set count of outgoing edges
-			thrust::fill(thrust::device, d_current_out_edges_count.begin(), d_current_out_edges_count.end(), 0);
+			thrust::fill(thrust::device, d_current_out_edges_count.begin(),
+					d_current_out_edges_count.end(), 0);
 			thrust::copy(thrust::device,
 					thrust::make_permutation_iterator(d_out_edges_count.begin(),
-												      d_near_set.begin()),
+							d_near_set.begin()),
 					thrust::make_permutation_iterator(d_out_edges_count.begin(),
-												      d_near_set.end()),
-					thrust::make_permutation_iterator(d_current_out_edges_count.begin(),
-													  d_near_set.begin()));
+							d_near_set.end()),
+					thrust::make_permutation_iterator(
+							d_current_out_edges_count.begin(),
+							d_near_set.begin()));
 
 			// Get number of outgoing edges
 			int num_out_edges = thrust::reduce(thrust::device,
-											   d_current_out_edges_count.begin(),
-											   d_current_out_edges_count.end());
+					d_current_out_edges_count.begin(),
+					d_current_out_edges_count.end());
 
 			// Scan count to get map
 			thrust::exclusive_scan(thrust::device,
-								   d_current_out_edges_count.begin(),
-								   d_current_out_edges_count.end(),
-								   d_current_out_edges_index.begin());
+					d_current_out_edges_count.begin(),
+					d_current_out_edges_count.end(),
+					d_current_out_edges_index.begin());
+#if DEBUG_MSG
+			std::cout << "Current outgoing vertices count: " << std::endl;
+			thrust::copy(d_current_out_edges_count.begin(),
+					d_current_out_edges_count.end(),
+					std::ostream_iterator<int>(std::cout, " "));
+			std::cout << std::endl;
+
+			std::cout << "Current outgoing vertices index: " << std::endl;
+			thrust::copy(d_current_out_edges_index.begin(),
+					d_current_out_edges_index.end(),
+					std::ostream_iterator<int>(std::cout, " "));
+			std::cout << std::endl;
+#endif
 
 			// Expanded source vertices
 			DVector d_exp_source_vertices(num_out_edges);
-			cusp::offsets_to_indices(d_current_out_edges_index, d_exp_source_vertices);
+			cusp::offsets_to_indices(d_current_out_edges_index,
+					d_exp_source_vertices);
+#if DEBUG_MSG
+			std::cout << "Source Vertices: " << std::endl;
+			thrust::copy(d_exp_source_vertices.begin(),
+					d_exp_source_vertices.end(),
+					std::ostream_iterator<int>(std::cout, " "));
+			std::cout << std::endl;
+#endif
 
 			// Scatter to put row offset in position
 			DVector d_frontier_position(num_out_edges, 1);
-			thrust::scatter_if(thrust::device,
-							   d_graph.row_offsets.begin(),
-					  	  	   d_graph.row_offsets.end() - 1,
-					  	  	   d_current_out_edges_index.begin(),
-					  	  	   d_current_out_edges_count.begin(),
-					  	  	   d_frontier_position.begin());
+			thrust::scatter_if(thrust::device, d_graph.row_offsets.begin(),
+					d_graph.row_offsets.end() - 1,
+					d_current_out_edges_index.begin(),
+					d_current_out_edges_count.begin(),
+					d_frontier_position.begin());
 
 			// Inclusive scan by key to sum get final frontier positions and values
 			thrust::inclusive_scan_by_key(thrust::device,
-										  d_exp_source_vertices.begin(),
-										  d_exp_source_vertices.end(),
-										  d_frontier_position.begin(),
-										  d_frontier_position.begin());
+					d_exp_source_vertices.begin(), d_exp_source_vertices.end(),
+					d_frontier_position.begin(), d_frontier_position.begin());
+#if DEBUG_MSG
+			std::cout << "Frontier Index: " << std::endl;
+			thrust::copy(d_frontier_position.begin(), d_frontier_position.end(),
+					std::ostream_iterator<int>(std::cout, " "));
+			std::cout << std::endl;
+#endif
+
+			// Get frontier vertices
+			DVector d_frontier_vertices(num_out_edges);
+			thrust::copy(thrust::device,
+					thrust::make_permutation_iterator(
+							d_graph.column_indices.begin(),
+							d_frontier_position.begin()),
+					thrust::make_permutation_iterator(
+							d_graph.column_indices.begin(),
+							d_frontier_position.end()),
+					d_frontier_vertices.begin());
+#if DEBUG_MSG
+			std::cout << "Frontier Vertices: " << std::endl;
+			thrust::copy(d_frontier_vertices.begin(), d_frontier_vertices.end(),
+					std::ostream_iterator<int>(std::cout, " "));
+			std::cout << std::endl;
+#endif
 
 			// Tentative distance
 			DVector d_tent_distance(num_out_edges);
 			thrust::transform(thrust::device,
 					thrust::make_permutation_iterator(d_graph.values.begin(),
-													  d_frontier_position.begin()),
+							d_frontier_position.begin()),
 					thrust::make_permutation_iterator(d_graph.values.begin(),
-													  d_frontier_position.end()),
+							d_frontier_position.end()),
 					thrust::make_permutation_iterator(d_distance.begin(),
-													  d_exp_source_vertices.begin()),
-					d_tent_distance.begin(),
-					thrust::plus<int>());
+							d_exp_source_vertices.begin()),
+					d_tent_distance.begin(), thrust::plus<int>());
+#if DEBUG_MSG
+			std::cout << "Tentative Distance: " << std::endl;
+			thrust::copy(d_tent_distance.begin(), d_tent_distance.end(),
+					std::ostream_iterator<int>(std::cout, " "));
+			std::cout << std::endl;
+#endif
+
+			// Remove if tent_dist[frontier vertex] > dist[frontier vertex]
+			ZipTuple2TentDistIterator zip_removed_end;
+			zip_removed_end = thrust::remove_if(thrust::device,
+					thrust::make_zip_iterator(
+							thrust::make_tuple(d_frontier_vertices.begin(),
+									d_tent_distance.begin())),
+					thrust::make_zip_iterator(
+							thrust::make_tuple(d_frontier_vertices.end(),
+									d_tent_distance.end())),
+					is_tent_dist_greater(d_distance.data().get()));
+
+			Tuple2TentDist tent_dist_iter =
+					zip_removed_end.get_iterator_tuple();
+			d_frontier_vertices.erase(thrust::get<0>(tent_dist_iter),
+					d_frontier_vertices.end());
+			d_tent_distance.erase(thrust::get<1>(tent_dist_iter),
+					d_tent_distance.end());
+
+			// Get key together maintaining increasing distance within pack of keys
+			thrust::sort_by_key(thrust::device, d_frontier_vertices.begin(),
+					d_frontier_vertices.end(), d_tent_distance.begin());
+
+			// For each key, get the minimum value
+			thrust::pair<DVectorIterator, DVectorIterator> new_end;
+			new_end = thrust::reduce_by_key(thrust::device,
+					d_frontier_vertices.begin(), d_frontier_vertices.end(),
+					d_tent_distance.begin(), d_frontier_vertices.begin(),
+					d_tent_distance.begin(), thrust::equal_to<int>(),
+					thrust::minimum<int>());
+
+			d_frontier_vertices.erase(new_end.first, d_frontier_vertices.end());
+			d_tent_distance.erase(new_end.second, d_tent_distance.end());
+#if DEBUG_MSG
+			std::cout << "Update Frontier Vertices: " << std::endl;
+			thrust::copy(d_frontier_vertices.begin(), d_frontier_vertices.end(),
+					std::ostream_iterator<int>(std::cout, " "));
+			std::cout << std::endl;
+
+			std::cout << "Update Frontier Tent Dist: " << std::endl;
+			thrust::copy(d_tent_distance.begin(), d_tent_distance.end(),
+					std::ostream_iterator<int>(std::cout, " "));
+			std::cout << std::endl;
+#endif
+
+			// Update distances
+			thrust::copy(thrust::device, d_tent_distance.begin(),
+					d_tent_distance.end(),
+					make_permutation_iterator(d_distance.begin(),
+							d_frontier_vertices.begin()));
 
 			// Split frontier vertices between near and far pile
 			int count_current_near = thrust::count_if(thrust::device,
-					d_tent_distance.begin(), d_tent_distance.end(), is_light(upper_distance));
+					d_tent_distance.begin(), d_tent_distance.end(),
+					is_within_range(upper_distance));
 			int count_current_far = d_tent_distance.size() - count_current_near;
 
 			// Resize near and far pile
@@ -120,99 +219,75 @@ int main(int argc, char* argv[]) {
 			d_far_pile.resize(d_far_pile.size() + count_current_far);
 
 			// Separate indices for near and far pile
-			DVector near_frontier_idx(count_current_near);
-			DVector d_near_tent_distance(count_current_near);
-			DVector far_frontier_idx(count_current_far);
-			DVector d_far_tent_distance(count_current_far);
-			thrust::partition_copy(thrust::device,
-			      make_zip_iterator(make_tuple(d_frontier_position.begin(), d_tent_distance.begin())),
-			      make_zip_iterator(make_tuple(d_frontier_position.end(), d_tent_distance.end())),
-			      make_zip_iterator(make_tuple(near_frontier_idx.begin(), d_near_tent_distance.begin())),
-			      make_zip_iterator(make_tuple(far_frontier_idx.begin(), d_far_tent_distance.begin())),
-			      is_light_tuple(upper_distance));
+			zip_removed_end = thrust::partition(thrust::device,
+					make_zip_iterator(
+							make_tuple(d_frontier_vertices.begin(),
+									d_tent_distance.begin())),
+					make_zip_iterator(
+							make_tuple(d_frontier_vertices.end(),
+									d_tent_distance.end())),
+					is_within_range_tuple(upper_distance));
 
-			// Copy vertices to far pile
-			thrust::copy(thrust::device,
-					thrust::make_permutation_iterator(d_graph.column_indices.begin(), far_frontier_idx.begin()),
-					thrust::make_permutation_iterator(d_graph.column_indices.begin(), far_frontier_idx.end()),
-					(d_far_pile.end() - count_current_far));
+			tent_dist_iter = zip_removed_end.get_iterator_tuple();
 
 			// Copy vertices to near set
-			thrust::copy(thrust::device,
-					thrust::make_permutation_iterator(d_graph.column_indices.begin(), near_frontier_idx.begin()),
-					thrust::make_permutation_iterator(d_graph.column_indices.begin(), near_frontier_idx.end()),
-					d_near_set.begin());
+			thrust::copy(thrust::device, d_frontier_vertices.begin(),
+					thrust::get<0>(tent_dist_iter), d_near_set.begin());
 
-			// Update near set distances
-			// Remove if tent_dist[frontier vertex] > dist[frontier vertex]
-			ZipTupleTentDistIterator zip_removed_end;
-			zip_removed_end = thrust::remove_if(thrust::device,
-					thrust::make_zip_iterator(thrust::make_tuple(
-							d_near_set.begin(),
-							d_near_tent_distance.begin())),
-					thrust::make_zip_iterator(thrust::make_tuple(
-							d_near_set.end(),
-							d_near_tent_distance.end())),
-					is_tent_dist_greater(d_distance.data().get()));
-
-			TupleTentDist tent_dist_iter = zip_removed_end.get_iterator_tuple();
-			d_near_set.erase(thrust::get<0>(tent_dist_iter), d_near_set.end());
-			d_near_tent_distance.erase(thrust::get<1>(tent_dist_iter), d_near_tent_distance.end());
-
-			// Sort for increasing distance
-			thrust::sort_by_key(d_near_tent_distance.begin(),
-					d_near_tent_distance.end(),
-					d_near_set.begin());
-
-			// Get key together maintaining increasing distance within pack of keys
-			thrust::stable_sort_by_key(d_near_set.begin(),
-					d_near_set.end(),
-					d_near_tent_distance.begin());
-
-			// Get unique target vertices (they should have the minimum distance)
-			thrust::pair<DVectorIterator, DVectorIterator> new_end;
-			new_end = thrust::unique_by_key(d_near_set.begin(),
-					d_near_set.end(),
-					d_near_tent_distance.begin());
-
-			d_near_set.erase(new_end.first, d_near_set.end());
-			d_near_tent_distance.erase(new_end.second, d_near_tent_distance.end());
-
-			// Update distances
-			thrust::copy(thrust::device,
-					d_near_tent_distance.begin(),
-					d_near_tent_distance.end(),
-					make_permutation_iterator(d_distance.begin(), d_near_set.begin()));
+			// Copy vertices to far pile
+			thrust::copy(thrust::device, thrust::get<0>(tent_dist_iter),
+					d_frontier_vertices.end(),
+					(d_far_pile.end() - count_current_far));
 		}
 
-		is_work_left = !d_far_pile.empty();
-		if (is_work_left) {
-			// Compact (Remove vertices non-set start distance)
-			DVectorIterator new_far_pile_end;
-			new_far_pile_end = thrust::remove_if(thrust::device, d_far_pile.begin(),
-												 d_far_pile.end(), is_dist_non_set(d_distance.data().get()));
-			d_far_pile.erase(new_far_pile_end, d_far_pile.end());
+#if DEBUG_MSG
+		std::cout << "Near Pile Vertices: " << std::endl;
+		thrust::copy(d_near_set.begin(), d_near_set.end(),
+				std::ostream_iterator<int>(std::cout, " "));
+		std::cout << std::endl;
 
-			// Compact (Remove duplicates)
-			thrust::sort(thrust::device, d_far_pile.begin(), d_far_pile.end());
-			new_far_pile_end = thrust::unique(thrust::device, d_far_pile.begin(), d_far_pile.end());
-			d_far_pile.erase(new_far_pile_end, d_far_pile.end());
+		std::cout << "Far Pile Vertices: " << std::endl;
+		thrust::copy(d_far_pile.begin(), d_far_pile.end(),
+				std::ostream_iterator<int>(std::cout, " "));
+		std::cout << std::endl;
+#endif
 
-			// Copy all far to near, it will be split in the inner loop
-			d_near_set.resize(d_far_pile.size());
-			thrust::copy(thrust::device, d_far_pile.begin(), d_far_pile.end(), d_near_set.begin());
-			d_far_pile.clear();
+		i++;
+		lower_distance = i * k_delta;
+		upper_distance = (i + 1) * k_delta;
 
-			// Go to next upper distance
-			i++;
-		}
+		// Compact (Remove duplicates)
+		DVectorIterator new_compacted_end;
+		thrust::sort(thrust::device, d_far_pile.begin(), d_far_pile.end());
+		new_compacted_end = thrust::unique(thrust::device, d_far_pile.begin(),
+				d_far_pile.end());
+		d_far_pile.erase(new_compacted_end, d_far_pile.end());
+
+		// Compact (Remove vertices with distance < i*delta)
+		/*
+		new_compacted_end = thrust::remove_if(thrust::device,
+				d_far_pile.begin(), d_far_pile.end(),
+				is_less_than_lower(lower_distance));
+		d_far_pile.erase(new_compacted_end, d_far_pile.end());*/
+
+		// Separate indices for near and far pile
+		new_compacted_end = thrust::partition(thrust::device,
+				d_far_pile.begin(), d_far_pile.end(), is_dist_set_in_near_pile(d_distance.data().get(), upper_distance));
+
+		// Copy vertices to near set
+		d_near_set.resize(
+				thrust::distance(d_far_pile.begin(), new_compacted_end));
+		thrust::copy(thrust::device, d_far_pile.begin(), new_compacted_end,
+				d_near_set.begin());
+
+		// Remove near vertices from far pile
+		d_far_pile.erase(d_far_pile.begin(), new_compacted_end);
 	}
 
 	std::cout << "Distances: " << std::endl;
-	thrust::copy(d_distance.begin(), d_distance.end(), std::ostream_iterator<int>(std::cout, " "));
+	thrust::copy(d_distance.begin(), d_distance.end(),
+			std::ostream_iterator<int>(std::cout, " "));
 	std::cout << std::endl;
 
 	return 0;
 }
-
-
