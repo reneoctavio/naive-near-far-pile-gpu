59c67182222a98aec386508cd0108c2acadf49b5
Rene Octavio Queiroz Dias <reneoctavio@gmail.com>
2021-01-24 13:01:40 -0300

    docs: Fix no license
    Fixes #2

--- src/sssp.cu
+++ src/sssp.cu
@@ -1,8 +1,25 @@
-/*
+/* 
+ * This file is part of the Naive Near-Far Pile Algorithm.
+ * Copyright (c) 2015 René Octavio Queiroz Dias.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * https://github.com/reneoctavio/naive-near-far-pile-gpu
+ *
  * sssp.cu
  *
  *  Created on: Nov 29, 2015
- *      Author: reneoctavio
+ *      Author: René Octavio Queiroz Dias
  */
 
 #include "sssp.cuh"
